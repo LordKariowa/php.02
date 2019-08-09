5748bc276f390be0191763c60decd0c475f7e92d
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2019-08-08 21:47:52 -0300

    aula de web III
    primeira aula de WEB III

---
+++ cadastrar.php
@@ -0,0 +1,5 @@
+<title>Cadastro do Cliente</title>
+<h2>Nome:</h2>
+<h2>E-mail:</h2>
+<h2>Estado Civil:</h2>
+<h2>Sexo:</h2>
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<title>php</title>
 </head>
 <body>
-    <h2>hello,galaxy!</h2> 
+    <h2>Painel de Sistemas</h2>
+    <ul>
+        <li><form>Cadastrar Cliente</form></li>
+    <p>
+    <li><form>Consultar Cliente</form></li>
+    </ul>
 </body>
 </html>
